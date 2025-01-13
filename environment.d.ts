912c32f2609a0a35759643216e76680c325a66c7
Michal Kus <michal513717@gmail.com>
2025-01-13 20:17:33 +0100

    Switched to graphql yoga

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,7 @@ declare global {
         interface ProcessEnv {
             MONGODB_URI: string;
             MONGODB_DB_NAME: string;
+            WEATHERSTACK_API_KEY: string;
         }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
+import * as dotenv from 'dotenv';
+import * as log4js from 'log4js';
+import { configureLogger } from './utils/logger';
+import { getMongoClient } from './databases/mongoStore';
+import { createYoga } from 'graphql-yoga';
+import { loadSchemas } from './api';
+import { createServer } from 'http';
 
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI || '';
 
 async function main() {
-    
+    configureLogger();
+
+    const logger = log4js.getLogger("Main");
+
+    try {
+        const mongoClient = await getMongoClient(mongoUri);
+        const schema = await loadSchemas();
+        const yoga = createYoga({ schema });
+        const server = createServer(yoga);
+        
+        server.listen(port, () => {
+            logger.log(`Server is running on port ${port}`);
+        });
+
+    } catch (error) {
+        logger.error('Error starting the server', error);
+    }
 }
 
 main().catch((error) => {
