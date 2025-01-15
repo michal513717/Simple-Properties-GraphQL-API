78a5979e35caf9d3c19d8041900e18cee0dec067
Michal Kus <michal513717@gmail.com>
2025-01-15 19:14:03 +0100

    Removed ts-ignore

--- src/api/resolvers/queries/propertyResolvers.resolver.ts
+++ src/api/resolvers/queries/propertyResolvers.resolver.ts
@@ -1,18 +1,17 @@
+import { PropertyQueryResolvers } from "../../../models/common.models";
 import { GetPropertiesService } from "../../../queries/getPropertiesService";
 import { GetPropertyByIdService } from "../../../queries/getPropertyByIdService";
 
-export const propertyQueryResolvers = {
-    //@ts-ignore
+export const propertyQueryResolvers: PropertyQueryResolvers = {
     getProperties: async (_, args) => {
         const service = new GetPropertiesService();
         const filters = args.filters || {};
-        const sortKey = args.sortKey || "createdAt";
+        const sortKey = args.sortKey || "";
         return await service.execute(filters, sortKey);
-      },
-      //@ts-ignore
-      getPropertyById: async (_, args) => {
+    },
+    getPropertyById: async (_, args) => {
         const service = new GetPropertyByIdService();
         return await service.execute(args.id);
-      },
-      hello: () => 'Hello World!',
-    }
+    },
+}
+
